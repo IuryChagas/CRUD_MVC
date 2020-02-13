e7c912a0a60198b14fd2a620f9433c8fd01a8258
Iury Chagas <iurychagas@icloud.com>
2020-02-13 14:09:18 -0300

    add set and launch Migrations

--- Database/ApplicationDBContext.cs
+++ Database/ApplicationDBContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using CRUD_MVC.Models;
 
 namespace CRUD_MVC.Database
 {
     public class ApplicationDBContext : DbContext
     {
+        public DbSet<Account> Accounts { get; set; }
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options){}
     }
 }
---
+++ Migrations/20200213165634_initAccount.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD_MVC.Migrations
+{
+    public partial class initAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Number = table.Column<int>(nullable: false),
+                    Holder = table.Column<string>(nullable: true),
+                    Balance = table.Column<double>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
